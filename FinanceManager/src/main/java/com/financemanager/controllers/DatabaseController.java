3214135ed56f420aad8b10f0704ef81458cf019b
Magnus Rødseth <magnus.rodseth@gmail.com>
2021-01-22 16:50:20 +0100

    Load application properties

---
+++ FinanceManager/src/main/java/com/financemanager/controllers/DatabaseController.java
@@ -0,0 +1,5 @@
+package com.financemanager.controllers;
+
+public interface DatabaseController {
+    // TODO: Develop methods to be implemented by the SQLController
+}
--- FinanceManager/src/main/java/com/financemanager/models/User.java
+++ FinanceManager/src/main/java/com/financemanager/models/User.java
@@ -3,12 +3,30 @@ package com.financemanager.models;
 import com.financemanager.utils.Email;
 
 public class User {
-    private int id;
+    private long id;
     private String firstName;
     private String lastName;
     private String email;
     private String password;
     private double balance;
+
+    public User(
+            long id,
+            String firstName,
+            String lastName,
+            String email,
+            String password,
+            double balance,
+            int accountNumber
+    ) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.balance = balance;
+        this.accountNumber = accountNumber;
+    }
 
     public int getAccountNumber() {
         return accountNumber;
@@ -16,7 +34,7 @@ public class User {
 
     private int accountNumber;
 
-    public int getId() {
+    public long getId() {
         return id;
     }
 
@@ -58,5 +76,18 @@ public class User {
 
     public void setBalance(double balance) {
         this.balance = balance;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", balance=" + balance +
+                ", accountNumber=" + accountNumber +
+                '}';
     }
 }
