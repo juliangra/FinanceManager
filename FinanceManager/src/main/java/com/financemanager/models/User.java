ecc6ea6b6a73517f502f4951d17fe5b1e6f18e81
Magnus Rødseth <magnus.rodseth@gmail.com>
2021-01-22 16:31:28 +0100

    Add User model

---
+++ FinanceManager/src/main/java/com/financemanager/models/User.java
@@ -0,0 +1,62 @@
+package com.financemanager.models;
+
+import com.financemanager.utils.Email;
+
+public class User {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private double balance;
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    private int accountNumber;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+}
